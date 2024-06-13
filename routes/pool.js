68fca77a82cd767c958432809c37c4bf3532f91e
naman0058 <42539761+naman0058@users.noreply.github.com>
2024-06-13 16:33:32 +0530

    Update pool.js

--- routes/pool.js
+++ routes/pool.js
@@ -6,7 +6,7 @@ const pool = mysql.createPool({
 
   host : 'db-mysql-blr1-86728-do-user-16935862-0.c.db.ondigitalocean.com',
   user: 'doadmin',
-  password: 'ss',
+  password: 'AVNS_KosX5wqBesuNqHnZWao',
     database: 'wordcreation',
     port:'25060' ,
     multipleStatements: true,
